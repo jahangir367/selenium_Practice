8686bd6aecc2af8cbb60a5db7be923b85ce44d9d
Shaik <shaik.jahangir.basha@macys.com>
2019-10-24 12:37:20 +0530

    Ïnitial Commit

---
+++ src/main/java/Selenium/Practice/com/Nisum/App.java
@@ -0,0 +1,15 @@
+package Selenium.Practice.com.Nisum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello 22222222222222!" );
+
+    }
+}
