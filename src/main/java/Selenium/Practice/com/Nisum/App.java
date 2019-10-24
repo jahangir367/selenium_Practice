87aa96b99dbf3cc40ced1ba0ca2aa268b7954124
jahangir367 <shaikjahangir2019@gmail.com>
2019-10-24 15:05:32 +0530

    save

--- src/main/java/Selenium/Practice/com/Nisum/App.java
+++ src/main/java/Selenium/Practice/com/Nisum/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello test for commit!" );
+        System.out.println( "Hello test for !" );
 
 
     }
